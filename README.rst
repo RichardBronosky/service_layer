370311261aa5e849a53f9b5eb04f6bf4bf97434b
Richard Bronosky <richard@bronosky.com>
2015-03-11 04:57:47 -0400

    setuptools stuff

---
+++ README.rst
@@ -0,0 +1,63 @@
+####################
+Mobile Service Layer
+####################
+
+A lightweight, containerized API aggregator using only Python, Redis, and S3
+
+------------
+
+Overview
+--------
+
+If you've ever made a mobile app for a company that claims to have an API, you understand the difference between an API that was designed for mobile consumption and one that was not.
+
+Key Challenges faced by mobile developers
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#. Unnecessary authentication/authorization to retrieve non-sensitive data
+#. Overly verbose output
+#. Requiring multiple round trips to get all data facets
+#. Requiring multiple APIs (often from multiple providers)
+#. Pagination inconsistent with the app's requirements
+#. Post-processing required to apply business rules
+#. Slow delivery (often caused by realtime searching or computation)
+
+All of this work is done in concert on thousands of slow, memory constrained devices on slow networks (and implemented in a different language for each mobile platform). It's truly insane. But, no one wants to change their API, nor do they want to pay you to host another API. So what's the solution? The one I've come up with is a lightweight API aggregator. It's containerized (using Docker) and uses Amazon S3 for storage and delivery. It's just Python and Redis. It doesn't even require a web server because there is no request-time computation.
+
+
+
+Process Flow
+~~~~~~~~~~~~
+
+.. image:: http://i.imgur.com/458tEAD.png
+    :width: 554px
+    :align: center
+    :height: 294px
+    :alt: Process Flow Chart
+    :target: http://i.imgur.com/458tEAD.png
+
+Usage
+~~~~~
+
+#. Modify ``celeryconfig.py`` to point to your Redis server
+#. Define a ``config.yml`` file. (sample.yml has been provided)
+#. Define a parser. (a Python function that takes a string as an argument and returns a modified string.)
+#. Add AWS credentials
+#. Use the included tools to start the workers, flower and celerybeat
+
+
+Tools included in the project
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+-  ``msl_worker``
+
+   -  Starts a Celery worker pool
+
+-  ``msl_beat``
+
+   -  Starts a Celery worker pool with Celerybeat active
+
+-  ``msl_flower``
+
+   -  Starts a Celery monitoring and managing web app
+
---
+++ setup.py
@@ -0,0 +1,75 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='service_layer',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.0',
+
+    description='A lightweight, containerized API aggregator using only Python, Redis, and S3',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/richardbronosky/service_layer',
+
+    # Author details
+    author='Bruno Bronosky',
+    author_email='bruno@bronosky.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Message Queues',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='celery redis boto celery_test',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['celery[redis]', 'pyyaml', 'flower', 'boto'],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'msl_worker=service_layer.console_scripts:worker',
+            'msl_beat=service_layer.console_scripts:beat',
+            'msl_flower=service_layer.console_scripts:flower',
+        ],
+    },
+)
